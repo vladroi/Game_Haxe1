bea2103583b25e31f222feaf2e429ac652993b10
Vlad Roi <vladroi@mail.ru>
2023-07-26 22:26:33 +0500

    move hero

--- src/Main.hx
+++ src/Main.hx
@@ -1,38 +1,50 @@
+import hxd.Key;
 import haxe.io.Float32Array;
 import hxd.FloatBuffer;
 
 class Main extends hxd.App{
-    var bmp : h2d.Bitmap;
+    var hero : h2d.Bitmap;
 
     var velocity : Float = 0;
     var gravity : Float = 10;
     
     var chunkSize : Float = 100;
+    
+    var windowsizex : Int = 800;
+    var windowsizey : Int = 768;
+
+    var herosizex : Int = 100;
+    var herosizey : Int = 100;
 
     override function init () {
        // create a white box + create 100x100 tile from it
-        var tile = h2d.Tile.fromColor(0xFFFFFF, 100, 100);
+        var tile = h2d.Tile.fromColor(0xFFFFFF, herosizex, herosizey);
         tile.dx = -tile.width * 0.5;
         tile.dy = -tile.height * 0.5;
         
        //create a bitmap jbject and add it to the default 2d scene (s2d)
-        bmp = new h2d.Bitmap(tile, s2d);
+        hero = new h2d.Bitmap(tile, s2d);
 
        //modify the display position of bitmap
-       bmp.x = s2d.width * 0.5;
-       bmp.y = s2d.height * 0.5;
+       hero.x = s2d.width * 0.5;
+       hero.y = s2d.height * 0.5;
     }
 
-    override function update(dt:Float) {
-        
-        bmp.y += velocity * chunkSize * dt;
-        velocity += gravity * dt;
-
-        if(bmp.y + bmp.tile.y/2 > s2d.height){
-            velocity = -velocity;
-        }
-
     
+
+    override function update(dt:Float){ 
+       MoveHero();
+    }
+
+    function MoveHero() {
+        if(Key.isDown(Key.A)&&(hero.x >= (herosizex/2)))
+            hero.x --;
+        if(Key.isDown(Key.D)&&(hero.x <= (windowsizex-(herosizex/2))))
+            hero.x ++;
+        if(Key.isDown(Key.W)&&(hero.y >= (herosizex/2)))
+            hero.y --;
+        if(Key.isDown(Key.S)&&(hero.y <= (windowsizey-(herosizey/2))))
+            hero.y ++;
     }
 
     static function main() {
